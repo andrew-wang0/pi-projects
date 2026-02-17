a1162ee1f2539aaa4342f50ea025abe8c558fdce
Andrew Wang <awwang3@uci.edu>
2026-02-17 15:53:58 -0800

    Initial commit

---
+++ packages/eslint-config/eslint.config.next.js
@@ -0,0 +1,14 @@
+import { globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
+
+import reactConfig from "./eslint.config.react.js";
+
+const config = [
+  ...nextVitals,
+  ...nextTs,
+  ...reactConfig,
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
+];
+
+export default config;
