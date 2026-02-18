32db71e0d51ec31589674a9e433bf518b04cc67b
Andrew Wang <awwang3@uci.edu>
2026-02-17 22:11:07 -0800

    chore: update manifest

--- apps/capy-messages/app/manifest.ts
+++ apps/capy-messages/app/manifest.ts
@@ -14,21 +14,14 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#000000",
     icons: [
       {
-        src: "/favicon.ico",
-        sizes: "any",
-        type: "image/x-icon",
-      },
-      {
         src: "/icon-192x192.png",
         sizes: "192x192",
         type: "image/png",
-        purpose: "any",
       },
       {
         src: "/icon-512x512.png",
         sizes: "512x512",
         type: "image/png",
-        purpose: "maskable",
       },
     ],
   };
